d7f88e2417e26f172a46d10f50591e1936cd3217
Christina <beck_chri@web.de>
2020-11-15 15:52:50 +0100

    Add fonts and adapt main fragment layout

--- app/src/main/java/com/laibold/myburger/model/Burger.kt
+++ app/src/main/java/com/laibold/myburger/model/Burger.kt
@@ -3,5 +3,5 @@ package com.laibold.myburger.model
 import com.laibold.myburger.model.ingredient.Ingredient
 
 data class Burger(var name: String, var ingredients: List<Ingredient>) {
-    var state = false
+
 }
